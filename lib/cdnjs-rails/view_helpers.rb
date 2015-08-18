e9c6b4c5b958c1a718477b188417f65b0a5c602c
Michael Varrieur <mike.varrieur@gmail.com>
2015-08-18 14:25:57 -0400

    Create a better window variable check than relying on jQuery

--- lib/cdnjs-rails/view_helpers.rb
+++ lib/cdnjs-rails/view_helpers.rb
@@ -3,15 +3,28 @@ module CDNJS
     def cdnjs_include_tag(cdn_vars=nil)
       cdn_vars       ||= Rails.application.config.cdnjs
       js_string_output = Array.new
-
+      
       cdn_vars.each do |js_file_config|
+        window_var = js_file_config.fetch(:windowvar)
+        split_vars = window_var.split(".")
+        window_path = ""
+
+        split_vars.each_with_index do |val, index|
+          var_check = ["window"]
+
+          0.upto(index) do |i|
+            var_check.push split_vars[i]
+          end
+
+          window_path << var_check.join(".") + " && "
+        end
+
         js_string_output << javascript_include_tag("//cdnjs.cloudflare.com/ajax/libs/#{js_file_config.fetch(:cdnjs)}")
-        js_string_output << javascript_tag("window.jQuery || document.write(unescape('#{asset_path(js_file_config.fetch(:localpath)).gsub('<','%3C')}'))")
+        js_string_output << javascript_tag("(#{window_path}) || document.write(unescape('#{asset_path(js_file_config.fetch(:localpath)).gsub('<','%3C')}'))")
       end
 
       js_string_output.join("\n").html_safe
     end
   end
 end
-
 
