baadb65570add030a07f01a5f4a8357b2dd88604
Michael Varrieur <mike.varrieur@gmail.com>
2015-08-18 21:07:50 -0400

    Properly form script tag

--- lib/cdnjs-rails/view_helpers.rb
+++ lib/cdnjs-rails/view_helpers.rb
@@ -22,7 +22,7 @@ module CDNJS
         window_path.chomp!(" && ")
 
         js_string_output << javascript_include_tag("//cdnjs.cloudflare.com/ajax/libs/#{js_file_config.fetch(:cdnjs)}")
-        js_string_output << javascript_tag("(#{window_path}) || document.write(unescape('#{asset_path(js_file_config.fetch(:localpath)).gsub('<','%3C')}'))")
+        js_string_output << javascript_tag("(#{window_path}) || document.write(unescape(\"%3Cscript src='#{asset_path(js_file_config.fetch(:localpath)).gsub('<','%3C')}' type='text/javascript'%3E%3C/script%3E\"))")
       end
 
       js_string_output.join("\n").html_safe
