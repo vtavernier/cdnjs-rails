984b2012431e0321b15629638e9c01c34fe24b33
Vincent Tavernier <vince.tavernier@gmail.com>
2017-09-08 01:43:19 +0200

    Add support for optional fallback

--- lib/cdnjs-rails/view_helpers.rb
+++ lib/cdnjs-rails/view_helpers.rb
@@ -5,24 +5,31 @@ module CDNJS
       js_string_output = Array.new
 
       cdn_vars.each do |js_file_config|
-        window_var = js_file_config.fetch(:windowvar)
-        split_vars = window_var.split(".")
-        window_path = ""
+        cdnjs_path = js_file_config.fetch(:cdnjs, nil)
+        window_var = js_file_config.fetch(:windowvar, nil)
+        local_path = js_file_config.fetch(:localpath, nil)
 
-        split_vars.each_with_index do |val, index|
-          var_check = ["window"]
+        # Output cdnjs loading tag
+        js_string_output << javascript_include_tag("//cdnjs.cloudflare.com/ajax/libs/#{cdnjs_path}")
 
-          0.upto(index) do |i|
-            var_check.push split_vars[i]
+        # Only print fallback code if fallback parameters have been specified
+        if window_var and local_path
+          split_vars = window_var.split(".")
+          window_path = ""
+
+          split_vars.each_with_index do |val, index|
+            var_check = ["window"]
+
+            0.upto(index) do |i|
+              var_check.push split_vars[i]
+            end
+
+            window_path << var_check.join(".") + " && "
           end
 
-          window_path << var_check.join(".") + " && "
+          window_path.chomp!(" && ")
+          js_string_output << javascript_tag("(#{window_path}) || document.write(unescape(\"%3Cscript src='#{asset_path(local_path).gsub('<','%3C')}' type='text/javascript'%3E%3C/script%3E\"))")
         end
-
-        window_path.chomp!(" && ")
-
-        js_string_output << javascript_include_tag("//cdnjs.cloudflare.com/ajax/libs/#{js_file_config.fetch(:cdnjs)}")
-        js_string_output << javascript_tag("(#{window_path}) || document.write(unescape(\"%3Cscript src='#{asset_path(js_file_config.fetch(:localpath)).gsub('<','%3C')}' type='text/javascript'%3E%3C/script%3E\"))")
       end
 
       js_string_output.join("\n").html_safe
